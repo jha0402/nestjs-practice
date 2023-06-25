8998fb3641773bd287471d52a7b65ead2a51c38c
jha0402 <67306416+jha0402@users.noreply.github.com>
2023-06-25 14:45:38 +0900

    Start new project: computer

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from './messages/messages.module';
+import { ComputerModule } from './computer/computer.module';
+import { DiskModule } from './disk/disk.module';
+import { CpuModule } from './cpu/cpu.module';
+import { PowerModule } from './power/power.module';
 
 @Module({
-  imports: [MessagesModule],
+  imports: [MessagesModule, ComputerModule, DiskModule, CpuModule, PowerModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ComputerModule } from './computer/computer.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(ComputerModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
