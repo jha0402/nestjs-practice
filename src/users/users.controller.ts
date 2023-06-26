affc66d42599f05afae5b2d4596e10966270b3eb
jha0402 <67306416+jha0402@users.noreply.github.com>
2023-06-26 10:42:44 +0900

    exclude response properties

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,9 @@ import {
   Query,
   Delete,
   Patch,
+  NotFoundException,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -20,9 +23,14 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.usersService.findOne(parseInt(id));
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return user;
   }
 
   @Get()
