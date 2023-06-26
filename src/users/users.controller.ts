626f9a6a7cd3dbe580f863a3ed68091fc416ca9d
jha0402 <67306416+jha0402@users.noreply.github.com>
2023-06-26 10:58:21 +0900

    remove exclusion

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,8 +8,6 @@ import {
   Delete,
   Patch,
   NotFoundException,
-  UseInterceptors,
-  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -23,7 +21,6 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
-  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
