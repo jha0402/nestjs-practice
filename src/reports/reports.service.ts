4bf7f04e342e23cfb2724756bcf6fbd3ff8cfb04
jha0402 <67306416+jha0402@users.noreply.github.com>
2023-06-28 15:25:16 +0900

    add get estimate report feature

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -10,12 +10,27 @@ import { GetEstimateDto } from './dtos/get-estimate.dto';
 export class ReportsService {
   constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
 
-  async createEstimate(estimateDto: GetEstimateDto) {
+  async createEstimate({
+    make,
+    model,
+    lng,
+    lat,
+    year,
+    mileage,
+  }: GetEstimateDto) {
     return this.repo
       .createQueryBuilder()
-      .select('*')
-      .where('make = :make', { make: estimateDto.make })
-      .getMany();
+      .select('AVG(price)', 'price')
+      .where('make = :make', { make })
+      .andWhere('model = :model', { model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year })
+      .andWhere('approved IS TRUE')
+      .orderBy('ABS(mileage - :mileage)', 'DESC')
+      .setParameters({ mileage })
+      .limit(3)
+      .getRawOne();
   }
 
   create(reportDto: CreateReportDto, user: User) {
